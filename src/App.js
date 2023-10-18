67e59edaf780d5cf35c0a1f0e29cf80eb36dddcc
jrukeijakare <juniorjoe567@gmail.com>
2023-10-18 08:59:32 +0300

    update login page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 //import '../public/images/home-phones-2x.png'
-import Image from 'react-bootstrap/Image';
 import { LoginSection } from './components/LoginSection';
 import { LoginImageSection } from './components/LoginImageSection';
 
 function App() {
   
-
+var username = "Joseph";
   
 
   return (
-    <div className="row" style={{marginRight:'0px',marginLeft:'0px'}}>
-      <LoginImageSection></LoginImageSection>
-      <LoginSection></LoginSection>
-    </div>
+    <>
+      <div className="row" style={{marginRight:'0px',marginLeft:'0px'}}>
+        <LoginImageSection></LoginImageSection>
+        <LoginSection username={username}></LoginSection>
+      </div>
+      <div className="row" style={{marginRight:'0px',marginLeft:'0px', marginBottom:'10px'}}>
+        <div className='col-md-12 text-center'>
+          <small className='mar-right'>Meta</small>
+          <small className='mar-right'>About</small>
+          <small className='mar-right'>Blog</small>
+          <small className='mar-right'>Jobs</small>
+          <small className='mar-right'>Help</small>
+          <small className='mar-right'>API</small>
+          <small className='mar-right'>Privacy</small>
+          <small className='mar-right'>Terms</small>
+          <small className='mar-right'>Location</small>
+          <small className='mar-right'>Instagram Lite</small>
+          <small className='mar-right'>Threads</small>
+          <small className='mar-right'>Contact Uploading & Non-Users</small>
+          <small className='colorgray'>Meta Verified</small>
+        </div>
+      </div>
+
+      <div className="row" style={{marginRight:'0px',marginLeft:'0px', marginBottom:'30px'}}>
+        <div className='col-md-12 text-center'>
+          <small className='mar-right'>English</small>
+          <small className='colorgray'>© 2023 Instagram from Meta</small>
+        </div>
+      </div>
+    </>
+    
   );
 }
 
--- src/components/LoginSection.js
+++ src/components/LoginSection.js
@@ -1,28 +1,77 @@
-import React,{Component} from 'react';
+import React from 'react';
 import Image from 'react-bootstrap/Image';
 import Form from 'react-bootstrap/Form';
+import TextField from '@mui/material/TextField';
+import Button from 'react-bootstrap/Button';
 
-export function LoginSection() {
-
-    var imageStyle = {
-    }
-
+export function LoginSection(props) {
     return(
         <div className='col-md-4'>
-            <div style={{border:'1px solid #dbdbdb',padding:'20px',margin:'50px 0px', display:'flex', justifyContent:'center'}} className=''>
+            <div style={{border:'1px solid #dbdbdb',padding:'20px',margin:'40px 40px 0px 0px', display:'flex', justifyContent:'center'}} className=''>
                
                <div className='row'>
                 <div className='col-md-12'>
-                    <Image src='../images/ajlEU-wEDyo.png' rounded style={imageStyle}/>
+                    <div className='imageStyle'></div>
                 </div>
+                <div className='col-md-12 mt-3 text-center d-grid gap-2'>
+                    <TextField id="outlined-basic" label="Username or Phone number" variant="outlined" size="small" />
+                </div>
+                <div className='col-md-12 mt-3 text-center d-grid gap-2'>
+                    <TextField id="outlined-basic" label="Password" variant="outlined" size="small" />
+                </div>
+                <div className='col-md-12 mt-3 text-center d-grid gap-2'>
+                <Button variant="primary" size="sm">
+                    Small button
+                </Button>
+                </div>
+
                 <div className='col-md-12 mt-3'>
-                    <Form.Control type="text" placeholder="Phone Number or Email" />
+                    <div className='row'>
+                        <div className='col-md-5'><hr/></div>
+                        <div className='col-md-2'>OR</div>
+                        <div className='col-md-5'><hr/></div>
+                    </div>
+                </div>
+                <div className='col-md-12 mt-3 text-center'>
+                    <span>Login with Facebook</span>
+                </div>
+                <div className='col-md-12 mt-3 text-center'>
+                    <small>Forgot password?</small>
                 </div>
                </div>
                             
             </div>
-
-            
+            <div style={{border:'1px solid #dbdbdb',padding:'20px',margin:'10px 40px 0px 0px', display:'flex', justifyContent:'center'}} className=''>
+               
+               <div className='row'>
+                
+                <div className='col-md-12 text-center'>
+                    <span>Don't have an account? Sign up</span>
+                </div>
+                
+               </div>
+                            
+            </div>
+            <div style={{margin:'10px 0px', display:'flex', justifyContent:'center'}} className=''>
+               
+               <div className='row'>
+                
+                <div className='col-md-12 text-center'>
+                    <small>Get the app.</small>
+                </div>
+                <div className='col-md-12 mt-3'>
+                    <div className='row'>
+                        <div className='col-md-6' style={{textAlign:"right"}}>
+                        <Image src='../images/googleplay.png' style={{width: "80%"}}/>
+                        </div>
+                        <div className='col-md-6'>
+                        <Image src='../images/microsoft.png' style={{width: "67%"}}/>
+                        </div>
+                    </div>
+                </div>
+               </div>
+                            
+            </div>
         </div>
     )
 }
